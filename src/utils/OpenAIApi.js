9a8c9aeb0117743cd2a98259152cb51689b25548
Василий Морарь <vasili-morari@MacBook-Air-Vasilij.local>
2023-08-26 12:07:10 +0300

    update-readmy and change-apikey

--- src/utils/OpenAIApi.js
+++ src/utils/OpenAIApi.js
@@ -1,58 +1,59 @@
-import axios from 'axios'
-const AUDIO_URL = 'https://api.openai.com/v1/audio/transcriptions';
-const CHAT_URL = 'https://api.openai.com/v1/chat/completions';
-const API_KEY = process.env.REACT_APP_APISECRET.split(',').reverse().join('');
-// const API_KEY = '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'
+import axios from "axios";
+const AUDIO_URL = "https://api.openai.com/v1/audio/transcriptions";
+const CHAT_URL = "https://api.openai.com/v1/chat/completions";
+// const API_KEY = process.env.REACT_APP_APISECRET.split(',').reverse().join('');
+const API_KEY = "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";
 
 export const sendAudio = async (formData) => {
   try {
     const response = await axios.post(AUDIO_URL, formData, {
       headers: {
-        'Authorization': `Bearer ${API_KEY}`,
+        Authorization: `Bearer ${API_KEY}`,
       },
     });
 
     if (!response.data.text) {
-      throw new Error('Ошибка при отправке аудио');
+      throw new Error("Ошибка при отправке аудио");
     }
-    console.log(response.data.text)
+    console.log(response.data.text);
 
     return response.data.text;
-
   } catch (error) {
-    console.error('Ошибка отправке:', error.message);
-    throw new Error('Произошла ошибка при отправке аудио');
+    console.error("Ошибка отправке:", error.message);
+    throw new Error("Произошла ошибка при отправке аудио");
   }
-}
+};
 
 export const sendText = async (text) => {
   try {
-    const response = await axios.post(CHAT_URL, {
-      model: 'gpt-3.5-turbo',
-      messages: [
-        {
-          role: 'user',
-          content: `Сделай текст лучше: '${text}'. В своем ответе не забудь вставить знаки перевода строки`,
-        },
-      ],
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${API_KEY}`,
+    const response = await axios.post(
+      CHAT_URL,
+      {
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content: `Сделай текст лучше: '${text}'. В своем ответе не забудь вставить знаки перевода строки`,
+          },
+        ],
       },
-    });
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${API_KEY}`,
+        },
+      }
+    );
 
-    console.log(response.data.choices[0].message.content)
+    console.log(response.data.choices[0].message.content);
 
     if (!response.data.choices[0].message.content) {
-      throw new Error('Ошибка при отправке текста');
+      throw new Error("Ошибка при отправке текста");
     }
 
     return response.data.choices[0].message.content;
-
   } catch (error) {
-    console.error('Ошибка при отправке текста:', error.message);
-    throw new Error('Произошла ошибка при отправке текста');
+    console.error("Ошибка при отправке текста:", error.message);
+    throw new Error("Произошла ошибка при отправке текста");
   }
-}
-
+};
